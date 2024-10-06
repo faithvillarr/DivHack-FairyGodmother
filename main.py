52240c3278c32c566aec2c7c3690396cd2cb0f43
faith.villarr <villa.faith21@gmail.com>
2024-10-05 20:48:31 -0400

    Reogranizing for Flask

---
+++ main.py
@@ -0,0 +1,20 @@
+# Our main Flask file.
+
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return redirect(url_for('outfit_board'))
+
+@app.route('/outfitboard')
+def outfit_board():
+    return render_template('outfitboard.html')
+
+@app.route('/outfitupload')
+def main():
+    return render_template('outfitupload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
