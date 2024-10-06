058142a75e799a8321f9de896258187090a052ff
faith.villarr <villa.faith21@gmail.com>
2024-10-06 02:23:27 -0400

    Developing picture picker.

--- choose_items.py
+++ choose_items.py
@@ -2,7 +2,34 @@
 # 2. iterate through each type, finding the item that most closely matches the palette
 # 3. return the chosen set of items
 
-# NOTE: two functions left to implement: reading items and palette from database, and comparing hex vals <3
+# NOTE: two functions left to implement: reading items and palette from database, and (DONE)comparing hex vals <3
+
+def hex_color_delta(hex1, hex2):
+    # Get red/green/blue int values of hex1
+    r1 = int(hex1[0:2], 16)
+    g1 = int(hex1[2:4], 16)
+    b1 = int(hex1[4:6], 16)
+    
+    # Get red/green/blue int values of hex2
+    r2 = int(hex2[0:2], 16)
+    g2 = int(hex2[2:4], 16)
+    b2 = int(hex2[4:6], 16)
+    
+    # Calculate differences between reds, greens, and blues
+    r = 255 - abs(r1 - r2)
+    g = 255 - abs(g1 - g2)
+    b = 255 - abs(b1 - b2)
+    
+    # Limit differences between 0 and 1
+    r /= 255
+    g /= 255
+    b /= 255
+    
+    return (r + g + b) / 3
+
+def pull_from_database():
+    return 1
+
 
 def find_closest_matches(palette):
     lists = pull_from_database() # tops {item: hex}, bottoms, dresses, shoes, outerwear, accessories
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Our main Flask file.
-from db_management import get_clothing_names, grab_closet_urls
-from flask import Flask, render_template, redirect, url_for, session
+from db_management import get_clothing_names, grab_closet_urls, upload_clothing_image
+from flask import Flask, render_template, redirect, url_for, session, request
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
@@ -22,6 +22,24 @@ def outfit_board():
 @app.route('/outfitupload')
 def main():
     return render_template('outfitupload.html')
+
+@app.route('/upload_clothing', methods=['POST'])
+def upload_clothing():
+    if 'fileInput' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['fileInput']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file:
+        filepath = upload_clothing_image(file_path, username, clothing_type)
+        file.save(filepath)
+        # Optionally store the URL in session or database for future display
+        session['closet_urls'].append(url_for('static', filename=f'uploads/{file.filename}'))
+    
+    return redirect(url_for('outfit_board'))
 
 if __name__ == '__main__':
     app.run(debug=True)
